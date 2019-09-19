15929ba5d69334a0f86962915c7c668aa5ef0643
Andrés Verenzuela <verenzuela@gmail.com>
2019-09-18 22:41:18 -0500

    update project name

--- android/app/src/main/java/com/hotelsbyday/MainActivity.java
+++ android/app/src/main/java/com/hotelsbyday/MainActivity.java
@@ -1,4 +1,4 @@
-package com.hbd;
+package com.hotelsbyday;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "hbd";
+        return "HotelsByDay";
     }
 
     @Override
